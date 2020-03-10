068700ae616476c7d7d8e53f16d7c7ed414a200d
Victoria Hale <vic.hale74@gmail.com>
2020-03-10 13:46:22 -0700

    add imports to frontend

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
