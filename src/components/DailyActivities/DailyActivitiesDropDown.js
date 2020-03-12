90c10269ec8c30e65d22c7d20caef3fbcd087b0d
Victoria Hale <vic.hale74@gmail.com>
2020-03-12 16:57:12 -0700

    dropdown select

--- src/components/DailyActivities/DailyActivitiesDropDown.js
+++ src/components/DailyActivities/DailyActivitiesDropDown.js
@@ -1,32 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getDogs } from '../../actions/dogs'
+import Select from "react-dropdown-select";
 
-class DailyActivitiesDropDown extends React.Component {
+class DailyActivitiesDropDown extends Component {
+    componentDidMount(){
+        this.props.getDogs()
+    }
 
-    render(){
-        let dogs = this.props.state.dogs;
-        let optionItems = dogs.map(dog => 
-            <option key={dog.name}>{dog.name}</option>
-            );
-
-            return (
-                <div>
-                    <select>
-                        {optionItems}
-                    </select>
-                </div>
-            )
+    render() {
+      
+        return(
+            <div>
+                <Select> 
+                {this.props.dogs.map((dog) => <options key={dog.id} value={dog.name}>Select a dog </options>)}
+                </Select>
+            </div>
+        )
     }
 }
 
 const mapStateToProps = state => {
     return{
-        dogs: state.dogReducer.dogs
+        dogs: state.dogReducer.dogs,
+        loading: state.dogReducer.loading
     }
 }
 
+export default connect(mapStateToProps, {getDogs})(DailyActivitiesDropDown);
 
 
 
-export default connect(mapStateToProps, { getDogs })(DailyActivitiesDropDown);
+
+
+// import React from 'react';
+// import { connect } from 'react-redux'
+// import { getDogs } from '../../actions/dogs'
+
+// class DailyActivitiesDropDown extends React.Component {
+//     componentDidMount(){
+//         let dogs = this.props.getDogs()
+//         console.log(dogs)
+//     }
+
+//     render(){
+//         let dogs = this.props.dogs;
+//         debugger
+//         let optionItems = dogs.dogs.map(dog => 
+//             <option key={dog.name}>{dog.name}</option>
+//             );
+
+//             return (
+//                 <div>
+//                     <select>
+//                         {optionItems}
+//                     </select>
+//                 </div>
+//             )
+//     }
+// }
+
+// const mapStateToProps = state => {
+//     return{
+//         dogs: state.dogReducer.dogs
+//     }
+// }
+
+// export default connect(mapStateToProps, { getDogs })(DailyActivitiesDropDown);
