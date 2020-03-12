ddb8d06663a127e5e9419fbf72bfaab6f8455995
Victoria Hale <vic.hale74@gmail.com>
2020-03-12 11:53:16 -0700

    cleaning up

--- src/components/DailyActivities/Form.js
+++ src/components/DailyActivities/Form.js
@@ -27,48 +27,43 @@ class Form extends Component {
        this.setState(this.initialState);
    }
 
-    submitForm = () => {
-        this.props.handleSubmit(this.state)
-        this.setState(this.initialState)
-    }
-
 
    render() {
        const {kind, name, time, description } = this.state;
 
         return (       
             <form onSubmit={this.onFormSubmit}>
-                <label for="kind">Kind</label>
-                <input> type="text" 
+                <label>Kind</label>
+                <input type="text" 
                             name="kind" 
                             id="name"
                             value={kind} 
-                            onChange={this.handleChange}</input>
-                <label for="name">Name</label>
-                <input> type="text" 
+                            onChange={this.handleChange}/>
+                <label>Name</label>
+                <input type="text" 
                             name="name" 
                             id="name"
                             value={name} 
-                            onChange={this.handleChange}</input>
-                <label for="name">Name</label>
-                <input> type="text" 
+                            onChange={this.handleChange}/>
+                <label>Name</label>
+                <input type="text" 
                             name="name" 
                             id="name"
                             value={name} 
-                            onChange={this.handleChange}</input>
-                <label for="time">Time</label>
-                <input> type="text" 
+                            onChange={this.handleChange}/>
+                <label>Time</label>
+                <input type="text" 
                             name="time" 
                             id="time"
                             value={time} 
-                            onChange={this.handleChange}</input>
-                <label for="description">Description</label>
-                <input> type="text" 
+                            onChange={this.handleChange}/>
+                <label>Description</label>
+                <input type="text" 
                             name="description" 
                             id="description"
                             value={description} 
-                            onChange={this.handleChange}</input> 
-                <input type="button" value="Submit" onClick={this.submitForm}></input>   
+                            onChange={this.handleChange}/> 
+                <button type="submit">Submit</button>  
             </form>
        )
    }
