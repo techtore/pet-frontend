4312cb5d397cd6e9689385f6db90ae3b74954776
Victoria Hale <vic.hale74@gmail.com>
2020-03-12 10:02:30 -0700

    add dog action

--- src/actions/dogs.js
+++ src/actions/dogs.js
@@ -7,3 +7,19 @@ export const getDogs = () => {
     }
 }
 
+export const addDog = (dog) => {
+    return(dispatch) => {
+        dispatch({type: "ADD_DOG"})
+        return fetch('http://localhost:3000/api/v1/dogs', {
+            method: 'POST',
+            body: JSON.stringify(dog),
+            headers: { 
+                'Content-Type':  'application/json'
+            }
+        })
+         .then(res => res.json())
+         .then(dog => {
+             return dispatch({ type: "DOG_ADDED", payload: dog })
+         })  
+    }
+}
--- src/components/DailyActivities/DailyActivitiesDropDown.js
+++ src/components/DailyActivities/DailyActivitiesDropDown.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { connect } from "react-redux";
-import Dropdown from "react-dropdown";
-import dog from '../Dogs/dog';
+// import React from 'react';
+// import { connect } from "react-redux";
+// import Dropdown from "react-dropdown";
+// import dog from '../Dogs/dog';
 
-const useReactDropdown = true;
-const option = { value:  }
+// const useReactDropdown = true;
+// const option = { value:  }
 
-const DailyActivitiesDropDown = ({input}) => {
-    return(
-        <Dropdown
-            {...input}
-            options={[
-                {
-                    value:
-                }
-            ]}
-        />
+// const DailyActivitiesDropDown = ({input}) => {
+//     return(
+//         <Dropdown
+//             {...input}
+//             options={[
+//                 {
+//                     value:
+//                 }
+//             ]}
+//         />
 
-    )
-}
+//     )
+// }
 
-const mapStateToProps = state => {
-    return{
-        dogs: state.dogReducer.dogs 
-    }
-}
+// const mapStateToProps = state => {
+//     return{
+//         dogs: state.dogReducer.dogs 
+//     }
+// }
 
 
 
-export default connect(mapStateToProps, { dog })(DailyActivitiesDropDown);
+// export default connect(mapStateToProps, { dog })(DailyActivitiesDropDown);
