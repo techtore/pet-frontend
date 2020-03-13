3f847672b75e533ed8274c648a1dd6cee5811747
Victoria Hale <vic.hale74@gmail.com>
2020-03-12 17:01:57 -0700

    render components on DA showpage

--- src/components/DailyActivities/DailyActivitiesShow.js
+++ src/components/DailyActivities/DailyActivitiesShow.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import DailyActivitiesDropDown from './DailyActivitiesDropDown'
+import Form from './Form'
+import DailyActivitiesTable from './DailyActivitiesTable'
 
 class DailyActivitiesShow extends React.Component {
     render() {
         return (
             <div>
                 <DailyActivitiesDropDown/>
+                <br></br>
+                <Form/>
+                <DailyActivitiesTable />
             </div>
         )
     }
